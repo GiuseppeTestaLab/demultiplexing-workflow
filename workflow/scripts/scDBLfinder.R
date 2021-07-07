97e92ce36a4fad27d4f5c98c763e91137d4ba5d1
castaway1990 <davide.castaldi@ieo.it>
2021-07-07 14:34:53 +0200

    Added consensus call

---
+++ workflow/scripts/scDBLfinder.R
@@ -0,0 +1,35 @@
+
+
+
+
+library(scDblFinder)
+library(DropletUtils)
+
+
+
+filtered_matrix <- snakemake@input[["filtered_matrix"]]
+barcodesclass <- snakemake@output[["barcodesclass"]]
+scDblFinderpath <- snakemake@params[["scDblFinderpath"]]
+
+
+
+dir.create(scDblFinderpath, showWarnings = FALSE)
+
+
+## __2. Read data__
+
+
+sce <- read10xCounts(filtered_matrix)
+
+## __3. scDBLfinder__
+
+
+sce <- scDblFinder(sce)
+
+calls <- DataFrame(scDBLfinder=sce$scDblFinder.class, row.names=sce$Barcode)
+
+
+
+## __4. Writing result__
+
+write.table(calls, barcodesclass, sep="\t", col.names=F, quote=F)
